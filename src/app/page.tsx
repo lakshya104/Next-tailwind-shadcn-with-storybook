4b35388f13b60fbf8eaa8170a5e92a5e282784a8
lakshya104 <lakshya.vyas@anktech.co.in>
2024-04-02 12:11:13 +0530

    Datatable component integrated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,112 @@ import TwButton from "@/stories/TwButton";
 import ShadcnBtn from "@/stories/ShadcnBtn";
 import { Mail } from "lucide-react";
 import { useState } from "react";
+import { DataTableDemo, Payment } from "@/stories/Table";
+
+const data: Payment[] = [
+  {
+    id: "cool",
+    amount: 316,
+    status: "success",
+    email: "ken99@yahoo.com",
+  },
+  {
+    id: "3u1reuv4",
+    amount: 242,
+    status: "success",
+    email: "Abe45@gmail.com",
+  },
+  {
+    id: "derv1ws0",
+    amount: 837,
+    status: "processing",
+    email: "Monserrat44@gmail.com",
+  },
+  {
+    id: "5kma53ae",
+    amount: 874,
+    status: "success",
+    email: "Silas22@gmail.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@hotmail.com",
+  },
+  {
+    id: "m5gr84i9",
+    amount: 316,
+    status: "success",
+    email: "ken99@yahoo.com",
+  },
+  {
+    id: "3u1reuv4",
+    amount: 242,
+    status: "success",
+    email: "Abe45@gmail.com",
+  },
+  {
+    id: "derv1ws0",
+    amount: 837,
+    status: "processing",
+    email: "Monserrat44@gmail.com",
+  },
+  {
+    id: "5kma53ae",
+    amount: 874,
+    status: "success",
+    email: "Silas22@gmail.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@hotmail.com",
+  },
+  {
+    id: "m5gr84i9",
+    amount: 316,
+    status: "success",
+    email: "ken99@yahoo.com",
+  },
+  {
+    id: "3u1reuv4",
+    amount: 242,
+    status: "success",
+    email: "Abe45@gmail.com",
+  },
+  {
+    id: "derv1ws0",
+    amount: 837,
+    status: "processing",
+    email: "Monserrat44@gmail.com",
+  },
+  {
+    id: "5kma53ae",
+    amount: 874,
+    status: "success",
+    email: "Silas22@gmail.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@hotmail.com",
+  },
+  {
+    id: "8nfhe5a7",
+    amount: 598,
+    status: "processing",
+    email: "johndoe@example.com",
+  },
+  {
+    id: "sdfr8943",
+    amount: 123,
+    status: "success",
+    email: "emma.smith@example.com",
+  },
+];
 
 export default function Home() {
   const [email, setEmail] = useState(false);
@@ -12,14 +118,15 @@ export default function Home() {
   const handleClick = () => {
     setEmail((prev) => !prev);
     setEmailTitle("Sending mail");
-    setTimeout(()=>{
-      setEmailTitle("Mail successfully sent") 
+
+    setTimeout(() => {
+      setEmailTitle("Mail sent successfully");
       setEmail((prev) => !prev);
-    },3000)
+    }, 3000);
   };
 
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen bg-white">
+    <div className="flex flex-col justify-center items-center bg-white">
       <p className="font-mono text-xl text-center"> Get a random color </p>
       <Button emoji="clap" />
       <TwButton title="TwButton" look="primary-outlined" />
@@ -27,11 +134,12 @@ export default function Home() {
         title={emailTitle}
         loader={email}
         icon={Mail}
-        variant="secondary"
+        variant="outline"
         iconClassName="mr-3"
-        className="my-3"
+        className="my-3 "
         onClick={handleClick}
       />
+      <DataTableDemo data={data} />
     </div>
   );
 }
---
+++ src/stories/assets/data.ts
@@ -0,0 +1,106 @@
+import { Payment } from "../Table";
+
+const data: Payment[] = [
+    {
+      id: "cool",
+      amount: 316,
+      status: "success",
+      email: "ken99@yahoo.com",
+    },
+    {
+      id: "3u1reuv4",
+      amount: 242,
+      status: "success",
+      email: "Abe45@gmail.com",
+    },
+    {
+      id: "derv1ws0",
+      amount: 837,
+      status: "processing",
+      email: "Monserrat44@gmail.com",
+    },
+    {
+      id: "5kma53ae",
+      amount: 874,
+      status: "success",
+      email: "Silas22@gmail.com",
+    },
+    {
+      id: "bhqecj4p",
+      amount: 721,
+      status: "failed",
+      email: "carmella@hotmail.com",
+    },
+    {
+      id: "m5gr84i9",
+      amount: 316,
+      status: "success",
+      email: "ken99@yahoo.com",
+    },
+    {
+      id: "3u1reuv4",
+      amount: 242,
+      status: "success",
+      email: "Abe45@gmail.com",
+    },
+    {
+      id: "derv1ws0",
+      amount: 837,
+      status: "processing",
+      email: "Monserrat44@gmail.com",
+    },
+    {
+      id: "5kma53ae",
+      amount: 874,
+      status: "success",
+      email: "Silas22@gmail.com",
+    },
+    {
+      id: "bhqecj4p",
+      amount: 721,
+      status: "failed",
+      email: "carmella@hotmail.com",
+    },
+    {
+      id: "m5gr84i9",
+      amount: 316,
+      status: "success",
+      email: "ken99@yahoo.com",
+    },
+    {
+      id: "3u1reuv4",
+      amount: 242,
+      status: "success",
+      email: "Abe45@gmail.com",
+    },
+    {
+      id: "derv1ws0",
+      amount: 837,
+      status: "processing",
+      email: "Monserrat44@gmail.com",
+    },
+    {
+      id: "5kma53ae",
+      amount: 874,
+      status: "success",
+      email: "Silas22@gmail.com",
+    },
+    {
+      id: "bhqecj4p",
+      amount: 721,
+      status: "failed",
+      email: "carmella@hotmail.com",
+    },
+    {
+      id: "8nfhe5a7",
+      amount: 598,
+      status: "processing",
+      email: "johndoe@example.com",
+    },
+    {
+      id: "sdfr8943",
+      amount: 123,
+      status: "success",
+      email: "emma.smith@example.com",
+    },
+  ];
